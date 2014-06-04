3722f8be048d209685d77a8adf7cea9bcebada40
Artem Sotnikov <artem.sotnikov@undev.ru>
2014-06-04 10:44:50 +0400

    init canvas images list

--- js/plastika.js
+++ js/plastika.js
@@ -1,4 +1,27 @@
-(function() {
+$(function(){
+  var canvasItem = $('.canvas_item');
+  var canvasList = $('.pl-canvas__pictlist');
+  var canvas = this.__canvas = new fabric.Canvas('c');
+  fabric.Object.prototype.transparentCorners = false;
 
+  canvasList.on('click', 'img', function(){
+    alert('ok');
+    var src = $(this).attr('src');
+    var idx = $(this).attr('id');
+    console.log($(this));
+  //   var left = Math.floor(Math.random(0, 900) * 800);
+  //   var top = Math.floor(Math.random(0, 700) * 700);
+  //   canvasList.hide();
 
-}).call(this);
+  //   fabric.loadSVGFromURL(src, function(objects, options) {
+  //     var loadedObject = fabric.util.groupSVGElements(objects, options);
+  //     loadedObject.set({
+  //       left: left,
+  //       top: top,
+  //       // angle: getRandomInt(-10, 10)
+  //     })
+  //     .setCoords();
+  //     canvas.add(loadedObject);
+  //   });
+  });
+})
